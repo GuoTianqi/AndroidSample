144e9752211e317d30ea13bd3f0b257ea6badc11
郭天祈 <guotianqi@star-net.cn>
2017-04-28 14:06:33 +0800

    modify MaterialDesign package add parentActivity

--- app/src/main/java/com/daybreak/androidsample/materialdesign/MaterialDesignActivity.java
+++ app/src/main/java/com/daybreak/androidsample/materialdesign/MaterialDesignActivity.java
@@ -1,88 +1,19 @@
 package com.daybreak.androidsample.materialdesign;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.daybreak.androidsample.R;
+import com.daybreak.androidsample.BaseClassListActivity;
+import com.daybreak.androidsample.coordinatorlayout.items.AppBarLayoutActivity;
+import com.daybreak.androidsample.coordinatorlayout.items.ClooapsingToolBarLayoutActivity;
+import com.daybreak.androidsample.coordinatorlayout.items.FloatingActionButtonActivity;
+import com.daybreak.androidsample.materialdesign.items.RecyclerViewActivity;
 
-import org.w3c.dom.Text;
-
-public class MaterialDesignActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+public class MaterialDesignActivity extends BaseClassListActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_material_design);
-
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        mRecyclerView.setHasFixedSize(true);
-
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        String[] myDataset = new String[] {
-                "Guo Tianqi",
-                "OO jlkj ",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
-                "OOOOONNN",
+    public Class[] getClassList() {
+        Class[] classes = new Class[] {
+                RecyclerViewActivity.class,
         };
-        mAdapter = new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
+        return classes;
     }
-}
-
-class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private String[] mDataset;
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mTextView;
-        public ViewHolder(View v) {
-            super(v);
-            mTextView = (TextView) v.findViewById(R.id.info_text);
-        }
-    }
-
-    public MyAdapter(String[] myDataset) {
-        mDataset = myDataset;
-    }
-
-    @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).
-                inflate(R.layout.my_text_view, parent, false);
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mTextView.setText(mDataset[position]);
-    }
-
-    @Override
-    public int getItemCount() {
-        return mDataset.length;
-    }
-
 }
---
+++ app/src/main/java/com/daybreak/androidsample/materialdesign/items/RecyclerViewActivity.java
@@ -0,0 +1,86 @@
+package com.daybreak.androidsample.materialdesign.items;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.daybreak.androidsample.R;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_design);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] myDataset = new String[] {
+                "Guo Tianqi",
+                "OO jlkj ",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+                "OOOOONNN",
+        };
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.info_text);
+        }
+    }
+
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.my_text_view, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+}
