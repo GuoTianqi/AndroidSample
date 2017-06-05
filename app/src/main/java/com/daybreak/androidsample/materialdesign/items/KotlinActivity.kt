7c723ced56ad921b8102d1154eccfc62599b09ee
郭天祈 <guotianqi@star-net.cn>
2017-06-05 18:03:59 +0800

    [ADD] move bitmap from mipmap to drawable

---
+++ app/src/main/java/com/daybreak/androidsample/materialdesign/items/KotlinActivity.kt
@@ -0,0 +1,14 @@
+package com.daybreak.androidsample.materialdesign.items
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.daybreak.androidsample.R
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
