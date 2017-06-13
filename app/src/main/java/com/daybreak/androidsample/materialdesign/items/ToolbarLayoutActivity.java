acc93d0418ffbe2dc7b1ef61f44a21160a6061fa
郭天祈 <guotianqi@star-net.cn>
2017-06-13 12:21:25 +0800

    [Modify] Toobar example

--- app/src/main/java/com/daybreak/androidsample/materialdesign/items/ToolbarLayoutActivity.java
+++ app/src/main/java/com/daybreak/androidsample/materialdesign/items/ToolbarLayoutActivity.java
@@ -1,9 +1,15 @@
 package com.daybreak.androidsample.materialdesign.items;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +19,7 @@ import android.widget.Toast;
 import com.daybreak.androidsample.R;
 
 public class ToolbarLayoutActivity extends AppCompatActivity {
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +37,53 @@ public class ToolbarLayoutActivity extends AppCompatActivity {
             }
         });
 
-
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_toolbar_layout, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Toast.makeText(ToolbarLayoutActivity.this, "Expaned", Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Toast.makeText(ToolbarLayoutActivity.this, "Collapse", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        Intent intent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText("Hello World!")
+                .setChooserTitle("OO")
+                .setSubject("HHH")
+                .createChooserIntent();
+
+        mShareActionProvider.setShareIntent(intent);
+
         return true;
     }
 
