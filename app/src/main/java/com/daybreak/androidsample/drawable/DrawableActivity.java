1e0f2b8391b60ef0142dbb30737619bef6db22ee
郭天祈 <guotianqi@star-net.cn>
2017-06-30 17:31:49 +0800

    [Add] DrawableActivity

---
+++ app/src/main/java/com/daybreak/androidsample/drawable/DrawableActivity.java
@@ -0,0 +1,16 @@
+package com.daybreak.androidsample.drawable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daybreak.androidsample.BaseToolBarActivity;
+import com.daybreak.androidsample.R;
+
+public class DrawableActivity extends BaseToolBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentLayout(R.layout.activity_drawable);
+    }
+}
