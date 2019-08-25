2ecfcd8ee7de1c9faff8ebc8182e23a1f884b1ee
Tianqi Guo <guotianqi1987@gmail.com>
2019-08-26 00:21:58 +0800

    [Modify] 补充完整opengl

--- app/src/main/java/com/daybreak/androidsample/opengl/simple/MyGLRenderer.kt
+++ app/src/main/java/com/daybreak/androidsample/opengl/simple/MyGLRenderer.kt
@@ -3,6 +3,8 @@ package com.daybreak.androidsample.opengl.simple
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import android.os.SystemClock
+import android.util.Log
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -12,11 +14,15 @@ class MyGLRenderer : GLSurfaceView.Renderer {
 
     // vPMatrix is an abbreviation for "Model View Projection Matrix"
     private val vPMatrix = FloatArray(16)
+    private val rotationMatrix = FloatArray(16)
+    private val scratchMatrix = FloatArray(16)
     private val projectionMatrix = FloatArray(16)
     private val viewMatrix = FloatArray(16)
 
+    @Volatile
+    var angle = 0f
 
-    override fun onDrawFrame(gl: GL10?) {
+    override fun onDrawFrame(gl: GL10) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
         // Set the camera position (View matrix)
@@ -25,8 +31,12 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
+//        val time = SystemClock.uptimeMillis() % 4000L
+//        val angle = 0.090f * time.toInt()
+        Matrix.setRotateM(rotationMatrix, 0, angle , 0f, 0f, -1.0f)
+        Matrix.multiplyMM(scratchMatrix, 0, vPMatrix, 0, rotationMatrix, 0)
         // Draw shape
-        mTriangle.draw(vPMatrix)
+        mTriangle.draw(scratchMatrix)
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
@@ -36,15 +46,24 @@ class MyGLRenderer : GLSurfaceView.Renderer {
 
         // this projection matrix is applied to object coordinates
         // in the onDrawFrame() method
-        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 9f)
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        // get exteninos
+        val extensions = gl?.glGetString(GL10.GL_EXTENSIONS)
+
+        Log.d(TAG, "opengl extension = $extensions")
+
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
 
         // initialize a triangle
         mTriangle = Triangle()
         // initialize a square
         mSquare = Square()
+    }
+
+    companion object {
+        val TAG = MyGLRenderer::class.java.simpleName
     }
 }
--- app/src/main/java/com/daybreak/androidsample/opengl/simple/MyGLSurfaceView.kt
+++ app/src/main/java/com/daybreak/androidsample/opengl/simple/MyGLSurfaceView.kt
@@ -2,18 +2,54 @@ package com.daybreak.androidsample.opengl.simple
 
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.opengl.GLSurfaceView.RENDERMODE_WHEN_DIRTY
 import android.util.AttributeSet
+import android.view.MotionEvent
+
+private const val TOUCH_SCALE_FACTOR = 180.0f / 320.0f
 
 class MyGLSurfaceView(context: Context, attrs: AttributeSet?) : GLSurfaceView(context, attrs) {
-        constructor(context: Context): this(context, null)
+    constructor(context: Context): this(context, null)
 
-        private val renderer : MyGLRenderer
+    private var previousX = 0f
+    private var previousY = 0f
 
-        init {
-            setEGLContextClientVersion(2)
+    private val renderer : MyGLRenderer
 
-            renderer = MyGLRenderer()
+    init {
+        setEGLContextClientVersion(2)
 
-            setRenderer(renderer)
-        }
+        renderer = MyGLRenderer()
+
+        setRenderer(renderer)
+
+        renderMode = RENDERMODE_WHEN_DIRTY
     }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+        when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                var dx = x - previousX
+                var dy = y - previousY
+
+                if (y > height / 2) {
+                    dx *= -1
+                }
+
+                if (x < width / 2) {
+                    dy *= -1
+                }
+
+                renderer.angle += (dx + dy) * TOUCH_SCALE_FACTOR
+                requestRender()
+            }
+        }
+
+        previousX = x
+        previousY = y
+
+        return true
+    }
+}
